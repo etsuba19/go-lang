1fe8f9bf9a6f53ce6a49e551b6203bd10296020f
Etsub Nadew <162500165+etsuba19@users.noreply.github.com>
2026-01-01 23:52:40 +0300

    Add files via upload

---
+++ Lab1/Codefile/variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a int = 10
+	var b float32 = 20.5
+	var c string = "Golang"
+	var d bool = true
+	e := "Short Declaration"
+
+	fmt.Println(a, b, c, d, e)
+	fmt.Println("Integer:", a)
+	fmt.Println("Float:", b)
+	fmt.Println("String:", c)
+	fmt.Println("Boolean:", d)
+	fmt.Println("Short Declaration:", e)
+}
